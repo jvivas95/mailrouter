b05d705c6d234a26ae260f9224df7ebc57f1b811
Jeff <jefferson.vivas.95@gmail.com>
2026-08-01 02:13:30 +0200

    feat: add AppConfig model

---
+++ app/Models/AppConfig.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class AppConfig extends Model
+{
+    //
+    protected $fillable = ['data'];
+
+    protected $casts = ['data' => 'array'];
+
+    public static function get(): array {
+
+        $config = static::first();
+        return $config ? $config->data : [];
+    }
+
+    public static function set(array $data): void {
+
+        static::updateOrCreate(['id' => 1], ['data' => $data]);
+    }
+}
