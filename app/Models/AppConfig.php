f9c43d230d210f9b98647537cf418cced50d61a6
Jeff <jefferson.vivas.95@gmail.com>
2026-08-04 18:05:29 +0200

    feat: define table name for AppConfig model

--- app/Models/AppConfig.php
+++ app/Models/AppConfig.php
@@ -7,6 +7,7 @@ use Illuminate\Database\Eloquent\Model;
 class AppConfig extends Model
 {
     //
+    protected $table = 'app_config';
     protected $fillable = ['data'];
 
     protected $casts = ['data' => 'array'];
